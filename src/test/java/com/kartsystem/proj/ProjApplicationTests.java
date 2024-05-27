2469ae7a4fdf9d9e1a02671dca6bcab94bdb3ca2
Vitor Vilela <vitorvilella123@gmail.com>
2024-05-27 13:01:08 -0300

    Commit teste

---
+++ src/test/java/com/kartsystem/proj/ProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kartsystem.proj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
